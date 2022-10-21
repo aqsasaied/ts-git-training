cc14b14056ea4bb5a955a8b9f02ed936bbe2935d
Aqsa Saied <Aqsa.Saied@softwire.com>
2022-10-21 16:30:02 +0100

    reset branch

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,11 +1,8 @@
-
 export const computeFibonacciNumber = (position: number | null): number => {
     let notNullPosition = position;
     if (notNullPosition === null) {
         notNullPosition = 1;   
     }
-    
-    if (isRecursive) return recursiveFibonacci(position);
 
     let i = 1;
     let j = 1;
@@ -23,14 +20,3 @@ export const computeFibonacciNumber = (position: number | null): number => {
     }
     return j;
 };
-
-const recursiveFibonacci = (initialPosition: number, left: number = 0, right: number = 1, position?: number): number => {
-    const currentPosition = position ?? initialPosition;
-    if (initialPosition === 0) return 0;
-    if (currentPosition === 0) return left;
-    if (initialPosition > 0) {
-        return recursiveFibonacci(initialPosition, right, left + right, currentPosition - 1);
-    } else {
-        return recursiveFibonacci(initialPosition, right - left, left, currentPosition + 1);
-    }
-}
